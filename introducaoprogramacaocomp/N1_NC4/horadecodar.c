0d7bb43cca65ad6be080860130b2767f75a754bb
Davis Xavier <130926303+davisxavier@users.noreply.github.com>
2026-02-24 13:31:00 +0000

    exercício codar

--- introducaoprogramacaocomp/N1_NC4/horadecodar.c
+++ introducaoprogramacaocomp/N1_NC4/horadecodar.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
 
+int main(){  
+    //DECLARAÇÃO DE VARIAVEL
+    int idade, matricula; 
+    float altura;
+    char nome [50];
+    //LEITURA DOS DADOS DO USUÁRIO
+    printf("Digite sua idade: \n");
+    scanf("%d", &idade);
+
+    printf("Digite sua altura: \n");
+    scanf("%f", &altura);
+
+    printf("Digite seu nome: \n");
+    scanf("%s", &nome);
+
+    printf("Digite sua matricula: \n");
+    scanf("%d", &matricula);
+    //DEVOLUÇÃO DO PROGRAMA
+    printf(" - Nome do aluno: %s \n - matricula: %d \n", nome, matricula);
+    printf(" - Idade: %d \n - Altura: %f \n", idade, altura);
+
+    return 0;
+}
